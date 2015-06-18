1ace3abb9042ef6db9a2f91e255638d7e5aabbc4
Elliot Lin <elliotlin@rocketmail.com>
2015-06-17 23:28:22 -0700

    Initial Commit

---
+++ Fridge Food/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fridge Food
+//
+//  Created by Elliot on 6/17/15.
+//  Copyright (c) 2015 Elliot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
